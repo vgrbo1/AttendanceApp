b06414edd4426a88f99f24d3a0e5e78153b49ddc
Vedad Grbo <vgrbo1@etf.unsa.ba>
2022-11-25 14:08:03 +0100

    Uradjena validacija prisustva

--- js/TabelaPrisustvo.js
+++ js/TabelaPrisustvo.js
@@ -1,3 +1,10 @@
 let TabelaPrisustvo = function (divRef, podaci) {
-    divRef.innerHTML = "TEST";
+    var prisustva = podaci.prisustva;
+
+    var brojPrisustvaPrevelik = prisustva.some(element =>
+        element.predavanja > podaci.brojPredavanjaSedmicno || element.vjezbe > podaci.brojVjezbiSedmicno
+    );
+
+    var brojPrisustvaNegativan = prisustva.some(element => element.predavanja < 0 || element.vjezbe < 0);
+
 };
--- js/prisustvo.js
+++ js/prisustvo.js
@@ -1,4 +1,4 @@
 let div = document.getElementById("divSadrzaj");
 let prisustvo = TabelaPrisustvo(div, {studenti: [{ime:"Neko",index:12345}],
 prisustva:[{sedmica:1,predavanja:1,vjezbe:1,index:12345}], predmet:"WT",
-brojPredavanjaSedmicno:3, "brojVjezbiSedmicno":2});
+brojPredavanjaSedmicno:3, brojVjezbiSedmicno:2});
