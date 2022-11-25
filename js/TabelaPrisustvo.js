d43df65950ea1f2a1f932aa12013f4ef27b5d303
Vedad Grbo <vgrbo1@etf.unsa.ba>
2022-11-25 15:38:05 +0100

    Provjera dva ili vise unosa za istu sedmicu studenta

--- js/TabelaPrisustvo.js
+++ js/TabelaPrisustvo.js
@@ -7,4 +7,9 @@ let TabelaPrisustvo = function (divRef, podaci) {
 
     var brojPrisustvaNegativan = prisustva.some(element => element.predavanja < 0 || element.vjezbe < 0);
 
+    var visePrisustvaZaIstuSedmicu = prisustva.some((element, index, array) => array.findIndex((element2) => 
+    element2.index == element.index && element2 == element.sedmica) != index);
+
+    if(visePrisustvaZaIstuSedmicu)
+        divRef.innerHTML = "NEISPRAVNO";
 };
