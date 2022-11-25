257efba447cf15d2e9cda9bde008a4eaec344575
Vedad Grbo <vgrbo1@etf.unsa.ba>
2022-11-25 16:13:40 +0100

    Provjera sedmica bez prisustva izmedju dvije sedmice

--- js/TabelaPrisustvo.js
+++ js/TabelaPrisustvo.js
@@ -16,4 +16,18 @@ let TabelaPrisustvo = function (divRef, podaci) {
     var indeksiPrisustva = prisustva.map((element) => element.index);
     var prisustvoNepostojecegStudenta = indeksiPrisustva.some((element) => indeksi.indexOf(element) == -1);
 
+    var sedmice = prisustva.map(element => element.sedmica);
+    sedmice = sedmice.filter((element,index,array) => {
+        return array.indexOf(element) == index
+    })
+    sedmice.sort((a,b) => a-b);
+
+    var nevalidneSedmice = false;
+    for(let i = 1; i < sedmice.length; i++){
+        if(sedmice[i]!=sedmice[i+1] + 1){
+            nevalidneSedmice = true;
+            break;
+        }
+    }
+
 };
