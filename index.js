d07bde48fe6c60f4ef771060e72af854464cf013
Vedad Grbo <vgrbo1@etf.unsa.ba>
2023-01-09 13:18:32 +0100

    Omoguceno redirektanje na /predmeti te logout

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const bcrypt = require ('bcrypt');
 const session = require("express-session");
+const path = require('path');
 
 app.use(express.static('public'));
 app.use(express.static('public/html'));
@@ -23,7 +24,6 @@ fs.readFile('data/nastavnici.json', (err, data) => {
 app.get('/', function(req, res){
     res.json({poruka: "OK"});
 });
-
 app.post('/login', function(req, res){
     let loginPodaci = req.body;
     nastavnik = nastavnici.find(n => n.nastavnik.username == loginPodaci.username);
@@ -36,9 +36,9 @@ app.post('/login', function(req, res){
             if (err) throw err;
 
             if (resp){ 
-                res.json({poruka: "Uspješna prijava"});
                 req.session.username = loginPodaci.username;
                 req.session.predmeti = nastavnik.predmeti;
+                res.json({poruka: "Uspješna prijava"});
             }
             else
                 res.json({poruka: "Neuspješna prijava"});
@@ -46,5 +46,23 @@ app.post('/login', function(req, res){
     }
 
     
-})
+});
+
+app.get('/predmeti', function(req, res){
+    if(req.session.username != undefined){
+        res.sendFile(path.join(__dirname,'public','html','predmeti.html'));
+    }
+    else{
+        res.json({poruka: "Nastavnik nije loginovan"});
+    }
+});
+app.post('/logout', function(req, res){
+    req.session.destroy(err => {
+        if (err) 
+          res.send(err);
+        else {
+            
+        }
+      });
+});
 app.listen(3000);
--- public/scripts/poziviAjax.js
+++ public/scripts/poziviAjax.js
@@ -12,12 +12,15 @@ const PoziviAjax = (()=>{
         ajax.send(JSON.stringify({username: username, password: password}));
         ajax.onreadystatechange = function () {
             if (ajax.readyState == 4 && ajax.status == 200){
-                callbackFja(null, ajax.responseText);
-                console.log(ajax.responseText);
+                window.location = "/predmeti";
             }
         };
     }
     function impl_postLogout(fnCallback){
+        let ajax = new XMLHttpRequest();
+        ajax.open("POST", "http://localhost:3000/logout", true);
+        ajax.send();
+        window.location = "/prijava.html";
     }
     //prisustvo ima oblik {sedmica:N,predavanja:P,vjezbe:V}
     function impl_postPrisustvo(naziv,index,prisustvo,fnCallback){
