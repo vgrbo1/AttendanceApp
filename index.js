48d47a81d0b1e49e0ef233361a6bd44b88afced0
Vedad Grbo <vgrbo1@etf.unsa.ba>
2023-01-20 00:45:12 +0100

    Radi sve samo finese doraditi

--- index.js
+++ index.js
@@ -41,14 +41,53 @@ const predmetModel = sequelize.define('predmet',{
     }
 }, {tableName: 'predmet'})
 
-nastavnikModel.hasMany(predmetModel, {as: 'predmeti', foreignKey: 'nastavnik_id'});
+const studentModel = sequelize.define('student', {
+    ime: {
+      type: Sequelize.STRING,
+    },
+    index: {
+      type: Sequelize.INTEGER,
+    }
+  },  {tableName: 'student'});
+
+const prisustvoModel = sequelize.define('prisustvo', {
+    sedmica: {
+      type: Sequelize.INTEGER,
+    },
+    predavanja: {
+      type: Sequelize.INTEGER,
+    },
+    vjezbe: {
+      type: Sequelize.INTEGER,
+    }
+  }, {tableName: 'prisustvo'});
+
+  studentModel.hasMany(prisustvoModel,{as:'prisustva'});
+  predmetModel.hasMany(prisustvoModel,{as: 'prisustva'});
+  nastavnikModel.hasMany(predmetModel, {as: 'predmeti'});
+
+
 const nastavnikBaza = {username: "USERNAME", password_hash: "$2b$10$KRK38RJ9rRkPjkr6WHI4lO5fwD9ptAOTRlYEiJC/tTxVd.5FZhGbq"};
-const predmet1 = {naziv: "Web tehnologije", brojPredavanjaSedmicno: 2, brojVjezbiSedmicno: 2, nastavnik_id: 1};
-const predmet2 = {naziv: "Tehnike programiranja", brojPredavanjaSedmicno: 2, brojVjezbiSedmicno: 2, nastavnik_id: 1};
+const predmet1 = {naziv: "Web tehnologije", brojPredavanjaSedmicno: 2, brojVjezbiSedmicno: 2, nastavnikId: 1};
+const predmet2 = {naziv: "Tehnike programiranja", brojPredavanjaSedmicno: 2, brojVjezbiSedmicno: 2, nastavnikId: 1};
+const student1 = {ime: "Neko Nekic", index: 12345};
+const student2 = {ime: "Drugi Neko", index: 12346};;
+const prisustvo1 = {sedmica: 1, predavanja: 0, vjezbe: 0, studentId: 1, predmetId: 1};
+const prisustvo2 = {sedmica: 1, predavanja: 0, vjezbe: 1, studentId: 2, predmetId: 1};
+const prisustvo3 = {sedmica: 2, predavanja: 1, vjezbe: 0, studentId: 1, predmetId: 1};
+const prisustvo4 = {sedmica: 2, predavanja: 1, vjezbe: 1, studentId: 2, predmetId: 1};
+
+const prisustvo5 = {sedmica: 1, predavanja: 0, vjezbe: 0, studentId: 1, predmetId: 2};
+const prisustvo6 = {sedmica: 1, predavanja: 0, vjezbe: 1, studentId: 2, predmetId: 2};
+const prisustvo7 = {sedmica: 2, predavanja: 1, vjezbe: 0, studentId: 1, predmetId: 2};
+const prisustvo8 = {sedmica: 2, predavanja: 1, vjezbe: 1, studentId: 2, predmetId: 2};
+
+const prisustvopredmet8 = {prisustvoId: 8, predmetId: 2};
 sequelize.sync({force: true}).then(async () =>{
     await nastavnikModel.create(nastavnikBaza);
-    await predmetModel.create(predmet1);
-    await predmetModel.create(predmet2);
+    await predmetModel.bulkCreate([predmet1,predmet2]);
+    await studentModel.bulkCreate([student1,student2]);
+    await prisustvoModel.bulkCreate([prisustvo1,prisustvo2,prisustvo3,prisustvo4,prisustvo5,prisustvo6,prisustvo7,prisustvo8]);
 });
 app.use(express.static('public'));
 app.use(express.static('public/html'));
@@ -59,7 +98,7 @@ app.use(session({
     saveUninitialized: true
  }));
  
-
+/*
 fs.readFile('data/nastavnici.json', (err, data) => {
     if (err) throw err;
     nastavnici = JSON.parse(data);
@@ -69,7 +108,7 @@ fs.readFile('data/prisustva.json', (err, data) => {
     if (err) throw err;
     prisustva = JSON.parse(data);
 });
-
+*/
 app.get('/', function(req, res){
     res.json({poruka: "OK"});
 });
@@ -95,26 +134,7 @@ app.post('/login', function(req, res){
               });
         }
     });
-    /*let nastavnik = nastavnici.find(n => n.nastavnik.username == loginPodaci.username);
-    
 
-    if(nastavnik == undefined)
-        res.json({poruka: "Neuspješna prijava"});
-    else{
-        bcrypt.compare(loginPodaci.password, nastavnik.nastavnik.password_hash, (err, resp) => {
-            if (err) throw err;
-
-            if (resp){ 
-                req.session.username = loginPodaci.username;
-                req.session.predmeti = nastavnik.predmeti;
-                res.json({poruka: "Uspješna prijava"});
-            }
-            else
-                res.json({poruka: "Neuspješna prijava"});
-          });
-    }
-
-    */
 });
 
 app.get('/predmeti', function(req, res){
@@ -126,18 +146,27 @@ app.get('/predmeti', function(req, res){
     }
 });
 
-app.get('/predmet/:NAZIV', function(req, res){
-    if(req.session.username != undefined){
-        let prisustvo = prisustva.find(p => p.predmet == req.params.NAZIV);
-        if(prisustvo != undefined){
-            res.json(prisustvo);
-        }
-        else{
-            res.status(404).json({poruka: "Nepostojeci predmet"});
-        }
+app.get('/predmet/:NAZIV', async function (req, res) {
+    if (req.session.username != undefined) {
+
+        let predmet = await predmetModel.findOne({ where: { naziv: req.params.NAZIV } });
+        let prisustva = await prisustvoModel.findAll({ where: { predmetId: predmet.id } });
+        let IDstudenata = [...new Set(prisustva.map(p => p.studentId))];
+        let studenti = await studentModel.findAll({ where: { id: IDstudenata } });
+
+        prisustva = prisustva.map(p => {
+            let index = studenti.find(s => s.id == p.studentId).index;
+            return { sedmica: p.sedmica, predavanja: p.predavanja, vjezbe: p.vjezbe, index: index };
+        });
+
+        res.json({
+            studenti: studenti, prisustva: prisustva, predmet: predmet.naziv, brojPredavanjaSedmicno: predmet.brojPredavanjaSedmicno,
+            brojVjezbiSedmicno: predmet.brojVjezbiSedmicno
+        });
+
     }
-    else{
-        res.json({poruka: "Nastavnik nije loginovan"});
+    else {
+        res.json({ poruka: "Nastavnik nije loginovan" });
     }
 });
 app.post('/logout', function(req, res){
@@ -150,38 +179,46 @@ app.post('/logout', function(req, res){
       });
 });
 
-app.post('/prisustvo/predmet/:NAZIV/student/:index', function(req, res){
+app.post('/prisustvo/predmet/:NAZIV/student/:index', async function(req, res){
     let naziv = req.params.NAZIV;
     let index = parseInt(req.params.index);
     let sedmica = req.body.sedmica;
     let predavanja = req.body.predavanja;
     let vjezbe = req.body.vjezbe;
 
-    let prisustvoPredmeta = prisustva.find(p => p.predmet == naziv);
-    if(prisustvoPredmeta == undefined){
+    let predmet = await predmetModel.findOne({where: {naziv: naziv}});
+    
+    if(predmet == undefined){
         res.status(404).json({poruka: "Nepostojeci predmet"});
     }
     else{
-        let student = prisustvoPredmeta.studenti.find(s => s.index == index);
+        let student = await studentModel.findOne({where: {index: index}});
         if(student == undefined){
             res.status(404).json({poruka: "Nepostojeci student"});
         }
         else{
-            let prisustvoStudenta = prisustvoPredmeta.prisustva.find(s => s.index == index && s.sedmica == sedmica);
+            let prisustvoStudenta = await prisustvoModel.findOne({where: {predmetId: predmet.id, studentId: student.id, sedmica: sedmica}});
             if (prisustvoStudenta == undefined){
-                prisustvoPredmeta.prisustva.push({sedmica: sedmica, predavanja: predavanja, vjezbe: vjezbe, index: index});
+                await prisustvoModel.create({predavanja: predavanja, vjezbe: vjezbe, studentId: student.id, sedmica: sedmica,
+                predmetId: predmet.id});
             }
             else{
-                prisustvoStudenta.predavanja = predavanja
-                prisustvoStudenta.vjezbe = vjezbe
+                await prisustvoModel.update({predavanja: predavanja, vjezbe: vjezbe},{where: {id: prisustvoStudenta.id}});
             }
-            fs.writeFile('data/prisustva.json', JSON.stringify(prisustva), (err) => {
-                if(err)
-                    console.log(err);
-                else{
-                    res.json(prisustvoPredmeta);
-                }
+            let prisustva = await prisustvoModel.findAll({ where: { predmetId: predmet.id } });
+            let IDstudenata = [...new Set(prisustva.map(p => p.studentId))];
+            let studenti = await studentModel.findAll({ where: { id: IDstudenata } });
+
+            prisustva = prisustva.map(p => {
+                let index = studenti.find(s => s.id == p.studentId).index;
+                return { sedmica: p.sedmica, predavanja: p.predavanja, vjezbe: p.vjezbe, index: index };
+                });
+
+            res.json({
+                studenti: studenti, prisustva: prisustva, predmet: predmet.naziv, brojPredavanjaSedmicno: predmet.brojPredavanjaSedmicno,
+                brojVjezbiSedmicno: predmet.brojVjezbiSedmicno
             });
+
         }
     }
 });
