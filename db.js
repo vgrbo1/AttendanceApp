644b7aa15563088464aad41861d842683c14ff82
Vedad Grbo <vgrbo1@etf.unsa.ba>
2023-04-06 23:16:09 +0200

    Added docker files

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('wt22', 'root', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-  });
-const db={};
+	host: 'mysql-db',
+	dialect: 'mysql',
+});
+const db = {};
 
-db.Sequelize = Sequelize;  
+db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.nastavnik = require(__dirname + '/model/nastavnik.js')(sequelize);
+db.predmet = require(__dirname + '/model/predmet.js')(sequelize);
+db.student = require(__dirname + '/model/student.js')(sequelize);
+db.prisustvo = require(__dirname + '/model/prisustva.js')(sequelize);
 
-db.nastavnik = require(__dirname+'/model/nastavnik.js') (sequelize);
-db.predmet = require(__dirname+'/model/predmet.js') (sequelize);
-db.student = require(__dirname+'/model/student.js') (sequelize);
-db.prisustvo = require(__dirname+'/model/prisustva.js') (sequelize);
+db.student.hasMany(db.prisustvo, { as: 'prisustva' });
+db.predmet.hasMany(db.prisustvo, { as: 'prisustva' });
+db.nastavnik.hasMany(db.predmet, { as: 'predmeti' });
 
-db.student.hasMany(db.prisustvo,{as:'prisustva'});
-db.predmet.hasMany(db.prisustvo,{as: 'prisustva'});
-db.nastavnik.hasMany(db.predmet, {as: 'predmeti'});
-
-module.exports=db;
+module.exports = db;
